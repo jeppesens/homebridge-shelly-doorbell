dfb02c8b2236bc36657316723c4c47ccb740c07b
sl1nd <gitlab@slind.de>
2021-04-01 00:06:29 +0200

    Now it goes

--- src/shelly-doorbell-accessory.ts
+++ src/shelly-doorbell-accessory.ts
@@ -182,7 +182,9 @@ export class ShellyDoorbell implements AccessoryPlugin {
   private async isDigitalDoorbellActive() {
     if (this._digitalDoorbellActive == null) {
       var config = await (await this.getStorage()).getItem(this.storageItemName);
-      this.log.info('Here we go: ' + config);
+      if (config === undefined) {
+        config = { digitalDoorbellActive: true };
+      }
       this._digitalDoorbellActive = config.digitalDoorbellActive;
     }
     return !!this._digitalDoorbellActive;
